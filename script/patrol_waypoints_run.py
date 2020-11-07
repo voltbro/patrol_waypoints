26fa59976f8973cfd2e56e4c14b3f12f2ebce564
Sorokonog <roman@voltbro.com>
2020-11-07 10:55:15 +0300

    cmd "next" and "prev" reworked

--- script/patrol_waypoints_run.py
+++ script/patrol_waypoints_run.py
@@ -17,7 +17,7 @@ class PatrolWaypointsManager():
     def __init__(self):
         rospy.init_node('patrol_waypoints_server')
         self.waypoints_list = []
-        self.wpselector = 0
+        self.curent_wp = -1
 
         self.patrol_waypoints_server() 
         # NO CODE BEYOND THIS LINE IN INIT
@@ -31,10 +31,12 @@ class PatrolWaypointsManager():
             wp = req.waypoint
             if req.num > len(self.waypoints_list):
                 self.waypoints_list.append(wp)
-                resp.response = "Waypoint added"
+                resp.response = "Waypoint appended"
+                rospy.loginfo("Waypoint appended")
             elif req.num <= 0:
                 self.waypoints_list.append(wp)
-                resp.response = "Waypoint added"
+                resp.response = "Waypoint appended"
+                rospy.loginfo("Waypoint appended")
             elif req.num < len(self.waypoints_list) and req.num > 0:
                 first_part_of_wp_list = self.waypoints_list[:(req.num-1)]
                 second_part_of_wp_list = self.waypoints_list[(req.num-1):]
@@ -55,7 +57,7 @@ class PatrolWaypointsManager():
             rospy.loginfo("Waypoint " + str(req.num) + " to go")
             resp.response = "Waypoint " + str(req.num) + " to go"
             resp.waypoint = [self.waypoints_list[req.num - 1]]
-            self.wpselector = req.num
+            self.curent_wp = req.num - 1
             return resp
         except Exception as err:
             rospy.loginfo(str(err))
@@ -92,7 +94,45 @@ class PatrolWaypointsManager():
             rospy.loginfo(str(err))
             resp.response = str(err)
             return resp
+
+    def next_wp(self, req):
+        resp = patrol_waypointsResponse()
+        try:
+            rospy.loginfo("Next waypoint")
+            resp.response = "Next waypoint"
+            if self.curent_wp == len(self.waypoints_list) - 1:
+                resp.waypoint = [self.waypoints_list[0]]
+                self.curent_wp = 0
+                resp.response = str(self.curent_wp + 1)
+            else:
+                resp.waypoint = [self.waypoints_list[self.curent_wp + 1]]
+                self.curent_wp += 1
+                resp.response = str(self.curent_wp + 1)
+            return resp
+        except Exception as err:
+            rospy.loginfo(str(err))
+            resp.response = str(err)
+            return resp
     
+    def prev_wp(self, req):
+        resp = patrol_waypointsResponse()
+        try:
+            rospy.loginfo("Previous waypoint")
+            resp.response = "Previous waypoint"
+            if self.curent_wp <= 0:
+                resp.waypoint = [self.waypoints_list[len(self.waypoints_list)-1]]
+                resp.response = str(len(self.waypoints_list))
+                self.curent_wp = len(self.waypoints_list) - 1
+            else:
+                resp.waypoint = [self.waypoints_list[self.curent_wp - 1]]
+                resp.response = str(self.curent_wp)
+                self.curent_wp -= 1
+            return resp
+        except Exception as err:
+            rospy.loginfo(str(err))
+            resp.response = str(err)
+            return resp
+
     def list_wp(self, req):
         resp = patrol_waypointsResponse()
         try:
@@ -105,40 +145,16 @@ class PatrolWaypointsManager():
             resp.response = str(err)
             return resp
 
-    def next_wp(self, req):
-        resp = patrol_waypointsResponse()
-        try:
-            rospy.loginfo("Next waypoint")
-            resp.response = "Next waypoint"
-            if self.wpselector < len(self.waypoints_list):
-                resp.waypoint = [self.waypoints_list[self.wpselector]]
-                self.wpselector += 1
-            else:
-                resp.waypoint = [self.waypoints_list[0]]
-                self.wpselector = 1
-            return resp
-        except Exception as err:
-            rospy.loginfo(str(err))
-            resp.response = str(err)
-            return resp
-
-    def load_wp(self, req):
-        rospy.loginfo("Waypoint file parsing started")
+    def home_wp(self, req):
         resp = patrol_waypointsResponse()
         wp = Pose2D()
+        wp.x = 0
+        wp.y = 0
+        wp.theta = 0 
         try:
-            waypoints_data_file = ('../data/'+ req.cmd.split()[1]) #TODO to point to correct dir
-            tree = ET.parse(waypoints_data_file)
-            root = tree.getroot()
-            i = 0
-            for waypoint in root.findall('waypoint'):
-                wp.x = float(waypoint.get('x'))
-                wp.y = float(waypoint.get('y'))
-                wp.theta = float(waypoint.get('theta'))
-                self.waypoints_list.append(copy.deepcopy(wp))
-            rospy.loginfo(self.waypoints_list)
-            resp.response = "Waypoints file succesfully loaded"
-            rospy.loginfo("Waypoints file succesfully loaded")
+            rospy.loginfo("Going Home")
+            resp.response = "Going Home"
+            resp.waypoint = [wp]
             return resp
         except Exception as err:
             rospy.loginfo(str(err))
@@ -164,6 +180,29 @@ class PatrolWaypointsManager():
 
             resp.response = "Waypoints file succesfully saved"
             rospy.loginfo("Waypoints file succesfully saved")
+            return resp
+        except Exception as err:
+            rospy.loginfo(str(err))
+            resp.response = str(err)
+            return resp
+
+    def load_wp(self, req):
+        rospy.loginfo("Waypoint file parsing started")
+        resp = patrol_waypointsResponse()
+        wp = Pose2D()
+        try:
+            waypoints_data_file = ('../data/'+ req.cmd.split()[1]) #TODO to point to correct dir
+            tree = ET.parse(waypoints_data_file)
+            root = tree.getroot()
+            i = 0
+            for waypoint in root.findall('waypoint'):
+                wp.x = float(waypoint.get('x'))
+                wp.y = float(waypoint.get('y'))
+                wp.theta = float(waypoint.get('theta'))
+                self.waypoints_list.append(copy.deepcopy(wp))
+            rospy.loginfo(self.waypoints_list)
+            resp.response = "Waypoints file succesfully loaded"
+            rospy.loginfo("Waypoints file succesfully loaded")
             return resp
         except Exception as err:
             rospy.loginfo(str(err))
